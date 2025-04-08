53035a3db20311c5f0ddf614b95a94e5a698c161
abd <raghman.gem@gmail.com>
2025-04-08 18:12:22 +0530

    c

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config) => {
+        config.module.rules.push({
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
