2254e32231dbbda434ac1697865bbae185e74043
abd <raghman.gem@gmail.com>
2025-06-05 01:02:51 +0530

    Contact change for google business profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,8 @@ export default function Footer() {
         <div className={styles.contact}>
           <h4>General Enquiries</h4>
           <Link href="mailto:sindhuskitchenusa@gmail.com" ><Image src="/asset/icon/email.png" alt="Sindhu's Kitchen" width={15} height={15} priority /> sindhuskitchenusa@gmail.com</Link>
-          <Link href="tel:+1-234-746-3487" ><Image src="/asset/icon/phone.png" alt="Sindhu's Kitchen" width={13} height={13} priority /> +1 234.SINDHUS</Link>
+          <Link href="tel:+12347463487" ><Image src="/asset/icon/phone.png" alt="Sindhu's Kitchen" width={13} height={13} priority /> +1 234-746-3487</Link>
+          <Link href="tel:+19402792536" ><Image src="/asset/icon/phone.png" alt="Sindhu's Kitchen" width={13} height={13} priority /> +1 940-279-2536</Link>
           <Link href="https://api.whatsapp.com/send/?phone=19402792536&text&type=phone_number&app_absent=0" target="_blank" className={styles.btn}><svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="WhatsAppIcon"><path d="M16.75 13.96c.25.13.41.2.46.3.06.11.04.61-.21 1.18-.2.56-1.24 1.1-1.7 1.12-.46.02-.47.36-2.96-.73-2.49-1.09-3.99-3.75-4.11-3.92-.12-.17-.96-1.38-.92-2.61.05-1.22.69-1.8.95-2.04.24-.26.51-.29.68-.26h.47c.15 0 .36-.06.55.45l.69 1.87c.06.13.1.28.01.44l-.27.41-.39.42c-.12.12-.26.25-.12.5.12.26.62 1.09 1.32 1.78.91.88 1.71 1.17 1.95 1.3.24.14.39.12.54-.04l.81-.94c.19-.25.35-.19.58-.11l1.67.88M12 2a10 10 0 0 1 10 10 10 10 0 0 1-10 10c-1.97 0-3.8-.57-5.35-1.55L2 22l1.55-4.65A9.969 9.969 0 0 1 2 12 10 10 0 0 1 12 2m0 2a8 8 0 0 0-8 8c0 1.72.54 3.31 1.46 4.61L4.5 19.5l2.89-.96A7.95 7.95 0 0 0 12 20a8 8 0 0 0 8-8 8 8 0 0 0-8-8z" fill='#fff'></path></svg> Whatsapp</Link>
         </div>
       </div>
